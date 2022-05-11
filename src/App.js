837a4a8c16d8864a1b53c8cc138d84a61aee4d7f
philquake <phillipcole123@gmail.com>
2022-05-11 17:26:12 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const App = () => {
 
     return (
         <div className="app">
-            <h1>BlockCuster</h1>
+            <h1>BlockGuster</h1>
 
             <div className="search">
                 <input
